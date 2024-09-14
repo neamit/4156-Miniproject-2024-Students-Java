a8813056164660d64c4f06c969afc689e9e58806
Neasha Mittal <neashamittal@Neashas-MacBook-Air.local>
2024-09-13 23:26:43 -0400

    adding files that were missed

--- IndividualProject/src/test/java/dev/coms4156/project/individualproject/CourseUnitTests.java
+++ IndividualProject/src/test/java/dev/coms4156/project/individualproject/CourseUnitTests.java
@@ -1,6 +1,8 @@
 package dev.coms4156.project.individualproject;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
@@ -8,7 +10,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
 
 /**
- * Class is a unit test class designed to test the behavior of the Course class using JUnit.
+ * Class is a unit test class designed to test the behavior of the Course class.
  */
 @SpringBootTest
 @ContextConfiguration
@@ -19,6 +21,8 @@ public class CourseUnitTests {
     testCourse = new Course("Griffin Newbold", "417 IAB", "11:40-12:55", 250);
   }
 
+  /** The test course instance used for testing. */
+  public static Course testCourse;
 
   @Test
   public void toStringTest() {
@@ -26,7 +30,38 @@ public class CourseUnitTests {
     assertEquals(expectedResult, testCourse.toString());
   }
 
-  /** The test course instance used for testing. */
-  public static Course testCourse;
+  @Test
+  public void enrollStudent() {
+    assertTrue(testCourse.enrollStudent());
+  }
+
+  @Test
+  public void dropStudent() {
+    testCourse.enrollStudent();
+    assertTrue(testCourse.dropStudent());
+  }
+
+  @Test
+  public void getCourseLocation() {
+    assertEquals("417 IAB", testCourse.getCourseLocation());
+  }
+
+  @Test
+  public void getInstructorNameTest() {
+    assertEquals("Griffin Newbold", testCourse.getInstructorName());
+  }
+
+  @Test
+  public void getCourseTimeSlotTest() {
+    assertEquals("11:40-12:55", testCourse.getCourseTimeSlot());
+  }
+
+  @Test
+  public void isCourseFull() {
+    if (testCourse.getEnrollmentCapacity() > testCourse.getEnrolledStudentCount()) {
+      assertTrue(testCourse.isCourseFull());
+    }
+  }
+
 }
 
